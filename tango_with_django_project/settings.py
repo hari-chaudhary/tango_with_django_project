09a5c212ae57cfe9b66eff244a6bac8f9f2bb6ea
hari_chaudhary <krisna00005@gmail.com>
2016-06-06 14:36:42 -0500

    implemented media directory

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -99,3 +99,6 @@ STATICFILES_DIRS = (
 
 STATIC_URL = '/static/'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # Absolute path to the media directory
+
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from django.conf import settings
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'tango_with_django_project.views.home', name='home'),
@@ -11,3 +13,10 @@ urlpatterns = patterns('',
 )
 
 
+# UNDERNEATH your urlpatterns definition, add the following two lines:
+if settings.DEBUG:
+    urlpatterns += patterns(
+        'django.views.static',
+        (r'^media/(?P<path>.*)',
+        'serve',
+        {'document_root': settings.MEDIA_ROOT}), )

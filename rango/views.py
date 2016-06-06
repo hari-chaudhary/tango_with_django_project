06af263f50a2efe2655e191441dfd807233adf93
hari_chaudhary <krisna00005@gmail.com>
2016-06-06 14:59:37 -0500

    implemented the about template page

--- rango/views.py
+++ rango/views.py
@@ -16,4 +16,8 @@ def index(request):
     return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-	return HttpResponse("Rango says here is the about page. <br/> <a href='/rango/'>Index</a>")
+
+	context_dict = {'boldmessage': "Rango's about page."}
+	
+	# return HttpResponse("Rango says here is the about page. <br/> <a href='/rango/'>Index</a>")
+	return render(request, 'rango/about.html', context_dict)

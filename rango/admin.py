41b0ec1812b8fc4cc1e04ce4e586e0d05ed349fb
hari_chaudhary <krisna00005@gmail.com>
2016-07-02 16:40:06 -0500

    user authentication implemented

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from rango.models import Category, Page
+from rango.models import Category, Page, UserProfile
 
 # Register your models here.
 # admin.site.register(Category)
@@ -13,3 +13,4 @@ class PageAdmin(admin.ModelAdmin):
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
+admin.site.register(UserProfile)
